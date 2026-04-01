5aea4ef99066909543ab0558148f0b2dc8100bcb
Thiago Carvalho <thiago.oak@gmail.com>
2026-04-01 20:26:58 -0300

    Add data-* and remove cljs
    Squashed commit of the following:
    
    commit d76072e0b239d311f133c64c1f34923dedc2d7ae
    Author: Thiago Carvalho <thiago.oak@gmail.com>
    Date:   Wed Apr 1 20:23:11 2026 -0300
    
        Remove CLJS
    
    commit 44845502782b03e450c9553ed13bcb401345c518
    Author: Thiago Carvalho <thiago.oak@gmail.com>
    Date:   Wed Apr 1 18:10:36 2026 -0300
    
        WIP datastar

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 ## Development
 ```shell
-npm i # install NPM deps
-npm run dev # run front-end dev build in watch mode with CLJS REPL
 clojure -M -m app.core # or run the server from REPL
 ```
 
--- src/app/core.clj
+++ src/app/core.clj
@@ -2,33 +2,53 @@
   (:require [org.httpkit.server :as http]
             [compojure.core :refer [defroutes GET POST]]
             [compojure.route :as route]
-            [uix.dom.server :as dom.server]
-            [uix.core :as uix :refer [defui $]]
             [datomic.api :as d]
+            [hiccup2.core :as h]
             [app.db]
             [clojure.data.json :as json])
   (:gen-class))
 
-(defui page [{:keys [title children]}]
-  ($ :html
-     ($ :head
-        ($ :title title)
-        ($ :meta {:name :viewport :content "width=device-width, initial-scale=1"})
-        ($ :link {:rel :preconnect :href "https://fonts.googleapis.com"})
-        ($ :link {:rel :preconnect :href "https://fonts.gstatic.com" :crossorigin true})
-        ($ :link {:rel :stylesheet :href "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"})
-        ($ :link {:rel :stylesheet :href "/main.css"}))
-     ($ :body
-        ($ :#root
-           children)
-        ($ :script {:src "/js/main.js"}))))
+(defn page [title children]
+  [:html
+   [:head
+    [:title title]
+    [:meta {:name :viewport :content "width=device-width, initial-scale=1"}]
+    [:link {:rel :preconnect :href "https://fonts.googleapis.com"}]
+    [:link {:rel :preconnect :href "https://fonts.gstatic.com" :crossorigin true}]
+    [:link {:rel :stylesheet :href "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"}]
+    [:link {:rel :stylesheet :href "/main.css"}]]
+   [:body [:div {:style {:height "100vh"
+                         :display :flex
+                         :flex-direction :column
+                         :gap 16
+                         :align-items :center
+                         :justify-content :center}}
+           [:h1 "Learn Datalog"]
+           [:div
+            [:p "Learn how to query a Datomic database with Datalog"]
+            [:p "@todo About the Schema"]
+            children]]
+    [:script {:type "module" :src "https://cdn.jsdelivr.net/gh/starfederation/datastar@1.0.0-RC.8/bundles/datastar.js"}]]])
 
 (defn run-q [req]
   (let [body (json/read-str (slurp (:body req)))
-        q (get body "q")
+        q (get body "input")
         conn (app.db/scratch-conn)]
     (app.db/setup-db conn)
     (str (d/q q (d/db conn)))))
+
+(defn runnable []
+  [:div {"data-signals:output" "'...'" "data-signals:input" "'[:find ?n :where [?e :pokemon/name ?n]]'"}
+   [:textarea {:name "input"
+               :class "input"
+               "data-bind" "input"
+               :style {:height 200 :width 300}}
+    "[:find ?n :where [?e :pokemon/name ?n]]"]
+   [:button {"data-on:click" "@post('/api/q')"} "run"]
+   [:textarea {:name "output"
+               :class "output"
+               "data-text" "$output"
+               :style {:height 200 :width 300}} ""]])
 
 (defroutes routes
   ;; In a real system, you would serve static files from a CDN
@@ -36,11 +56,13 @@
   ;; Api routes
   (POST "/api/q" req
     {:status 200
-     :headers {"Content-Type" "text/plain"}
-     :body (run-q req)})
-  ;; SPA mode: Renders HTML page for all routes
-  (GET "*" req
-    (dom.server/render-to-string ($ page {:title "Learn Datalog"}))))
+     :headers {"Content-Type" "application/json"}
+     :body (json/write-str {"output" (run-q req)})})
+  (GET "/" req
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :body (str (h/html
+                 (page "title" (runnable))))}))
 
 (defn run-server []
   (let [port (or (some-> (System/getenv "PORT") parse-long)
